388f90077ff91b4e7a994a6c9b53676b31d9ae78
Shane Bonham <bonham@ministrycentered.com>
2016-01-01 18:56:38 -0800

    Users never even had passwords

--- db/migrate/20151217231514_add_uid_to_user.rb
+++ db/migrate/20151217231514_add_uid_to_user.rb
@@ -2,6 +2,5 @@ class AddUidToUser < ActiveRecord::Migration
   def change
     add_column :users, :uid, :string
     remove_column :users, :monk_authentication_token, :string
-    remove_column :users, :password, :string
   end
 end
